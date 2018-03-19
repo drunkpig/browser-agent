2312c948ace1e6e9c680f213c385209acb929f03
Jon <xu_chao@chainatelecom.cn>
2018-03-19 20:41:42 +0800

    update

--- routes/page.js
+++ routes/page.js
@@ -5,25 +5,32 @@ const logger = require("../log").logger;
 
 /* GET users listing. */
 router.get('/:url', function (req, res, next) {
-    var url = req.params.url;
-    logger.info("function begin");
-    (async () => {
-        const browser = await puppeteer.launch();
-        logger.info("puppeteer.launch()");
-        const page = await browser.newPage();
-        await page.goto(url);
-        logger.info("page.goto(%s)", url);
-        const textContent = await page.evaluate(() => {
-            var page_content =  document.querySelector('html').innerHTML;
-            //logger.info("get url content ok");
-            return page_content;
-        });
-        logger.info("browser.close() begin");
-        await browser.close();
-        logger.info("send response");
-        res.send(textContent);
-    })();
-    logger.info("function end");
+    try{
+        var url = req.params.url;
+        logger.info("function begin");
+        (async () => {
+            const browser = await puppeteer.launch();
+            logger.info("puppeteer.launch()");
+            const page = await browser.newPage();
+            await page.goto(url);
+            logger.info("page.goto(%s)", url);
+            const textContent = await page.evaluate(() => {
+                var page_content =  document.querySelector('html').innerHTML;
+                //logger.info("get url content ok");
+                return page_content;
+            });
+            logger.info("browser.close() begin");
+            await browser.close();
+            logger.info("send response");
+            res.send(textContent);
+        })();
+        logger.info("function end");
+    }catch(err){
+        logger.info("function error %s", err)
+        res.statusCode = 500
+        res.send("");
+    }
+
 });
 
 module.exports = router;
